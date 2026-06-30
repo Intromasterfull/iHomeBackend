b8df2cfd10a84b9e759e3fafcf85b8cff06660d5
Fernando Banuelos <fernand_def_int@hotmail.com>
2026-06-30 01:09:35 -0600

    Actualizacion raspberry

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.use('/api', scheduleRoutes);
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log('Server running on port desde la raspberry');
 });
